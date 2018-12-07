d3565ceee548472b23af64313d5647398bd9e63a
Axel Eriksson <Axel.DTK.Eriksson@lystkom.se>
2018-12-07 11:08:41 +0100

    done with todo app.

--- projects/toDo/assets/js/todo.js
+++ projects/toDo/assets/js/todo.js
@@ -1,30 +1,36 @@
 
 
-var ToDolist = ["Hej"];
+var ToDolist = [];
 
 
 
-document.getElementsByTagName("button")[0], addEventListener("click" ,function(){
+document.getElementsByTagName("button")[0], addEventListener("click" ,function(){ // starts the while loop on button click
     var cool = [];
-    var input;
-    while (input != "quit" ) {
-        var userinput = prompt("what you wanna do?")
-        switch (userinput) {
-            case "new":
-                userinput = prompt("what you wanna add?");
-                ToDolist.push(userinput) // elliott
-                
-                break;
-            case "list":
+    var userinput;
+    while (userinput != "quit" ) {  // ends the app
+        var userinput = prompt("what you wanna do?")       
+        switch (userinput) {        // Use the switch statement to select one of many code blocks to be executed.
 
+
+            case "new":         // let me add a user.
+                userinput = prompt("what you wanna add?");
+                ToDolist.push(userinput)
+                this.console.log("you have added: " + userinput)                
                 break;
-            case "delete":
-                
+            case "list":            // shows a list of my "new " users.
+                ToDolist.forEach(function(myList){
+                    console.log(myList);
+                    console.log("#################")
+                });
                 break;
-            case "quit":
-                
+            case "delete":                  // delets the index i choose, remeber that it starts with 0.
+                userinput = prompt("What do you want to delete? ");
+                ToDolist.splice(userinput, 1); // 
+                console.log("you deleted index: " + userinput);
                 break;
+            
             default:
+                console.log("try again!")
                 break;
         }
     
