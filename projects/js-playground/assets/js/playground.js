7ee9fc6d65f31dbf0d023cf0d0e6676fe9e09ffe
Axel Eriksson <Axel.DTK.Eriksson@lystkom.se>
2019-04-24 11:17:25 +0200

    wtf

---
+++ projects/js-playground/assets/js/playground.js
@@ -0,0 +1,56 @@
+const p1Span = document.getElementById("player1");
+const p2Span = document.getElementById("player2");
+const p1Button = document.getElementById("p1");
+const p2Button = document.getElementById("p2");
+const inputScore = document.getElementById("scoreKeeper");
+const playToSpan = document.getElementById("scoreToWin");
+const resetButton = document.getElementsByTagName("button")[2];
+let gameOver = false;
+let p1ScoreCount = 0;
+let p2ScoreCount = 0;
+
+p1Button.addEventListener("click", () => {
+  p1ScoreCount++;
+  p1Span.textContent = p1ScoreCount;
+  if (p1ScoreCount == inputScore.value) {
+    alert("Player 1 Win!");
+    p1Span.className += " green";
+    p1Button.disabled = true;
+    p2Button.disabled = true;
+  }
+});
+
+p2Button.addEventListener("click", () => {
+  p2ScoreCount++;
+  p2Span.textContent = p2ScoreCount;
+  if (p2ScoreCount == inputScore.value) {
+    alert("Player 2 Win!");
+    
+    p2Span.className += " green";
+    p1Button.disabled = true;
+    p2Button.disabled = true;
+  }
+});
+
+inputScore.addEventListener("click", () => {
+  playToSpan.textContent = inputScore.value;
+});
+
+resetButton.addEventListener("click", function() {
+  reset();
+});
+
+function reset() {
+  gameOver = false;
+  p1ScoreCount = 0;
+  p2ScoreCount = 0;
+  p1Span.innerHTML = 0;
+  p2Span.innerHTML = 0;
+  inputScore.value = 5;
+  p1Span.classList.remove("green");
+  p2Span.classList.remove("green");
+  p1Button.disabled = false;
+  p2Button.disabled = false;
+}
+
+
--- projects/scorekeeper/assets/js/scorekeeper.js
+++ projects/scorekeeper/assets/js/scorekeeper.js
@@ -1,23 +1,54 @@
-var player1Button = document.getElementById("p1");
-var player1 = document.getElementById("player1");
-player1Score = 0;
+const p1Span = document.getElementById("player1");
+const p2Span = document.getElementById("player2");
+const p1Button = document.getElementById("p1");
+const p2Button = document.getElementById("p2");
+const inputScore = document.getElementById("scoreKeeper");
+const playToSpan = document.getElementById("scoreToWin");
+const resetButton = document.getElementsByTagName("button")[2];
+let gameOver = false;
+let p1ScoreCount = 0;
+let p2ScoreCount = 0;
 
-    player1Button.addEventListener("click", function(){
-        player1.innerHTML = player1Score++;
-    });
+p1Button.addEventListener("click", () => {
+  p1ScoreCount++;
+  p1Span.textContent = p1ScoreCount;
+  if (p1ScoreCount == inputScore.value) {
+    alert("Player 1 Win!");
+    p1Span.className += " green";
+    p1Button.disabled = true;
+    p2Button.disabled = true;
+  }
+});
 
-var player2Button = document.getElementById("p2");
-var player2 = document.getElementById("player2");
-player2Score = 0;
+p2Button.addEventListener("click", () => {
+  p2ScoreCount++;
+  p2Span.textContent = p2ScoreCount;
+  if (p2ScoreCount == inputScore.value) {
+    alert("Player 2 Win!");
+    
+    p2Span.className += " green";
+    p1Button.disabled = true;
+    p2Button.disabled = true;
+  }
+});
 
-    player2Button.addEventListener("click", function(){
-        player2.innerHTML = player2Score++;
-    });
+inputScore.addEventListener("click", () => {
+  playToSpan.textContent = inputScore.value;
+});
 
-var scoreButton = document.querySelector("score");
-var score = document.qu("score");
-player2Score = 0;
+resetButton.addEventListener("click", function() {
+  reset();
+});
 
-    scoreButton.addEventListener("click", function(){
-        score.innerHTML = player2Score++;
-    });
+function reset() {
+  gameOver = false;
+  p1ScoreCount = 0;
+  p2ScoreCount = 0;
+  p1Span.innerHTML = 0;
+  p2Span.innerHTML = 0;
+  inputScore.value = 5;
+  p1Span.classList.remove("green");
+  p2Span.classList.remove("green");
+  p1Button.disabled = false;
+  p2Button.disabled = false;
+}
