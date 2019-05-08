64de81399ebc8e250a1e9dc122afc88d3cf27415
Axel Eriksson <Axel.DTK.Eriksson@lystkom.se>
2019-05-08 08:55:13 +0200

    king

--- projects/js-playground/assets/js/playground.js
+++ projects/js-playground/assets/js/playground.js
@@ -54,3 +54,8 @@ function reset() {
 }
 
 
+function toggleDarkLight() {
+	var body = document.getElementById('body');
+	var currentClass = body.className;
+	body.className = currentClass == 'dark-mode' ? 'light-mode' : 'dark-mode';
+}
